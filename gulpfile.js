e5d2e12ee4e467cd2f0dd765d2f16617ffb9631d
Paul Lewis <gravatar@aerotwist.com>
2015-06-24 11:08:44 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -239,36 +239,3 @@ gulp.task('default', function() {
 gulp.task('dev', function() {
   return runSequence('clean', 'getversion', allTasks, 'watch');
 });
-
-// 'use strict';
-
-// var watchify = require('watchify');
-// var browserify = require('browserify');
-// var gulp = require('gulp');
-// var source = require('vinyl-source-stream');
-// var buffer = require('vinyl-buffer');
-// var gutil = require('gulp-util');
-// var assign = require('lodash.assign');
-
-// // add custom browserify options here
-// var customOpts = {
-//   entries: ['./src/scripts/guitartuner-core.js'],
-//   debug: true
-// };
-// var opts = assign({}, watchify.args, customOpts);
-// var b = watchify(browserify(opts));
-
-// // add transformations here
-// // i.e. b.transform(coffeeify);
-
-// gulp.task('js', bundle); // so you can run `gulp js` to build the file
-// b.on('update', bundle); // on any dep update, runs the bundler
-// b.on('log', gutil.log); // output build logs to terminal
-
-// function bundle() {
-//   return b.bundle()
-//     // log errors if they happen
-//     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-//     .pipe(source('bundle.js'))
-//     .pipe(gulp.dest('./dist'));
-// }
